2d40b7dd4374a844baeeb596627e530d43c62257
rdtyuihjkhgf <115413602+rdtyuihjkhgf@users.noreply.github.com>
2023-12-17 19:19:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function setup(){
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+}
+function clearCanvas(){
+    background("white")
+}
+function preload(){
+    classifier=ml5.imageClassifier('DoodleNet');
+}
+function draw(){
+    strokeWeight(15);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY)
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult);
+}
+function gotResult(error,result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+    }
+    document.getElementById('label').innerHTML='Label '+result[0].label;
+    document.getElementById('confidence').innerHTML='Confidence '+ Math.round(result[0].confidence*100)+'%';
+    utterThis=new SpeechSynthesisUtterance(result[0].label);
+    synth.speak(utterThis)
+}
